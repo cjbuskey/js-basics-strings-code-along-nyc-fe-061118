c68c04e8b2681cd45b62b44e21f994dbb1fe8b3e
clark.buskey@gmail.com <clark.buskey@gmail.com>
2018-07-16 23:00:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 currentUser.slice(0,1);
-const shortGreeting = `Welcome, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser}!`;
